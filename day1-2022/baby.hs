08f1b4ffe23fbb5a3e0838b40c59b78fe307febd
bashbunni <bashbunni.io@gmail.com>
2022-12-17 00:43:53 -0800

    day1

--- day1-2022/baby.hs
+++ day1-2022/baby.hs
@@ -1,3 +1,7 @@
 doubleMe x = x + x
 
-doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                         then x
+                         else doubleMe x
